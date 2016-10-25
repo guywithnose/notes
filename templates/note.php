804dc4caaabecc420e18a17bc9877ff3a23d2c63
Hendrik Leppelsack <hendrik@leppelsack.de>
2016-10-25 19:00:32 +0200

    increase contrast of meta data and add tooltip

--- templates/note.php
+++ templates/note.php
@@ -1,4 +1,9 @@
 <div id="app-navigation-toggle" class="icon-menu" style="display:none;"></div>
 	<pre editor notes-timeout-change="save()" notes-autofocus class="editor" ng-class="{saving: isSaving()}">{{note.content}}</pre>
-	<div class="note-meta">{{note.content | wordCount}}<span class="note-meta-right"><button class="icon-fullscreen" ng-click="toggleDistractionFree()"></button></span></div>
+	<div class="note-meta">
+		{{note.content | wordCount}}
+		<span class="note-meta-right">
+			<button class="icon-fullscreen has-tooltip" notes-tooltip ng-click="toggleDistractionFree()" title="test"></button>
+		</span>
+	</div>
 </div>
