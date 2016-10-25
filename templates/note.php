ae47d3c8ab1f16d5c6b717a39938b6d1c057b87a
Hendrik Leppelsack <hendrik@leppelsack.de>
2016-10-25 13:19:24 +0200

    add distraction free mode

--- templates/note.php
+++ templates/note.php
@@ -1,4 +1,4 @@
 <div id="app-navigation-toggle" class="icon-menu" style="display:none;"></div>
 	<pre editor notes-timeout-change="save()" notes-autofocus class="editor" ng-class="{saving: isSaving()}">{{note.content}}</pre>
-	<div class="note-meta">{{note.content | wordCount}}</div>
+	<div class="note-meta">{{note.content | wordCount}}<span class="note-meta-right"><button class="icon-fullscreen" ng-click="toggleDistractionFree()"></button></span></div>
 </div>
